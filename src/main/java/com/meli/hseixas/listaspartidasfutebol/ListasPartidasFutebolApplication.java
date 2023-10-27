a71a5b49c8123bffff15ebe10b3b09dae5e947fd
Hugo Seixas <hugo.seixas@mercadolivre.com>
2023-10-27 17:46:13 -0300

    Gerando projeto com Spring Initializr

---
+++ src/main/java/com/meli/hseixas/listaspartidasfutebol/ListasPartidasFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.meli.hseixas.listaspartidasfutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListasPartidasFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListasPartidasFutebolApplication.class, args);
+	}
+
+}
