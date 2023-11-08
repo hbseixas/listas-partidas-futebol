068bb63f30b186c193850d25b56d8df86f4bd0fc
Hugo Seixas <hugo.seixas@mercadolivre.com>
2023-11-08 18:07:52 -0300

    Criar CRUD

--- src/main/java/com/meli/hseixas/listaspartidasfutebol/controller/PartidaController.java
+++ src/main/java/com/meli/hseixas/listaspartidasfutebol/controller/PartidaController.java
@@ -1,9 +1,15 @@
 package com.meli.hseixas.listaspartidasfutebol.controller;
 
+import com.meli.hseixas.listaspartidasfutebol.dto.PartidaDto;
+import com.meli.hseixas.listaspartidasfutebol.model.Partida;
 import com.meli.hseixas.listaspartidasfutebol.service.PartidaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("partidas")
@@ -12,4 +18,34 @@ public class PartidaController {
     @Autowired
     private PartidaService partidaService;
 
+    @GetMapping
+    public ResponseEntity<List<PartidaDto>> listarTodasAsPartidas(){
+        var todasAsPartidas = partidaService.listarTodasAsPartidas();
+        return ResponseEntity.ok(todasAsPartidas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PartidaDto> listarPartidaPorId(@PathVariable Long id){
+        var partidaPorId = partidaService.listarPartidaPorId(id);
+        return ResponseEntity.ok(partidaPorId);
+    }
+
+    @PostMapping
+    public ResponseEntity<PartidaDto> cadastrarPartida(@RequestBody Partida partida, UriComponentsBuilder uriBuilder) {
+        var partidaCadastrada = partidaService.cadastrarPartida(partida);
+        URI uri = uriBuilder.path("/partidas/{id}").buildAndExpand(partidaCadastrada.getId()).toUri();
+        return ResponseEntity.created(uri).body(partidaCadastrada);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PartidaDto> alterarPartida(@PathVariable Long id, @RequestBody Partida partida) {
+        var partidaAlterada = partidaService.alterarPartida(id, partida);
+        return ResponseEntity.ok(partidaAlterada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Partida> excluirPartida(@PathVariable Long id) {
+        partidaService.excluirPartida(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/meli/hseixas/listaspartidasfutebol/service/PartidaService.java
+++ src/main/java/com/meli/hseixas/listaspartidasfutebol/service/PartidaService.java
@@ -1,8 +1,15 @@
 package com.meli.hseixas.listaspartidasfutebol.service;
 
+import com.meli.hseixas.listaspartidasfutebol.dto.PartidaDto;
+import com.meli.hseixas.listaspartidasfutebol.model.Partida;
 import com.meli.hseixas.listaspartidasfutebol.repository.PartidaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PartidaService {
@@ -10,5 +17,64 @@ public class PartidaService {
     @Autowired
     private PartidaRepository partidaRepository;
 
+    public List<PartidaDto> listarTodasAsPartidas() {
+        List<Partida> todasAsPartidas = partidaRepository.findAll();
+        if (!todasAsPartidas.isEmpty()){
+            return todasAsPartidas.stream()
+                    .map(this::converterParaDto)
+                    .toList();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
 
+    public PartidaDto listarPartidaPorId(Long id) {
+        Optional<Partida> partidaOptional = partidaRepository.findById(id);
+        if (partidaOptional.isPresent()) {
+            Partida partida = partidaOptional.get();
+            return converterParaDto(partida);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    public PartidaDto cadastrarPartida(Partida partida) {
+        partidaRepository.save(partida);
+        return converterParaDto(partida);
+    }
+
+    public PartidaDto alterarPartida(Long id, Partida novaPartida) {
+        Optional<Partida> partidaOptional = partidaRepository.findById(id);
+        if (partidaOptional.isPresent()){
+            Partida partida = partidaOptional.get();
+            partida.setClubeMandante(novaPartida.getClubeMandante());
+            partida.setClubeVisitante(novaPartida.getClubeVisitante());
+            partida.setPlacarMandante(novaPartida.getPlacarMandante());
+            partida.setPlacarVisitante(novaPartida.getPlacarVisitante());
+            partida.setNomeEstadio(novaPartida.getNomeEstadio());
+            partida.setHorario(novaPartida.getHorario());
+            partidaRepository.save(partida);
+            return converterParaDto(partida);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    public void excluirPartida(Long id) {
+        Optional<Partida> partidaOptional = partidaRepository.findById(id);
+        if (partidaOptional.isPresent()){
+            partidaRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public PartidaDto converterParaDto (Partida partida) {
+        PartidaDto partidaDto = new PartidaDto();
+        partidaDto.setId(partida.getId());
+        partidaDto.setClubeMandante(partida.getClubeMandante());
+        partidaDto.setClubeVisitante(partida.getClubeVisitante());
+        partidaDto.setPlacarMandante(partida.getPlacarMandante());
+        partidaDto.setPlacarVisitante(partida.getPlacarVisitante());
+        partidaDto.setHorario(partida.getHorario());
+        partidaDto.setNomeEstadio(partida.getNomeEstadio());
+        return partidaDto;
+    }
 }
