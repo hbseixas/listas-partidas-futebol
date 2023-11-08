120b69b920fec125fee182b563c5fe5fe3498228
Hugo Seixas <hugo.seixas@mercadolivre.com>
2023-11-08 14:04:49 -0300

    Adicionando atributos à PartidaDto

--- src/main/java/com/meli/hseixas/listaspartidasfutebol/dto/PartidaDto.java
+++ src/main/java/com/meli/hseixas/listaspartidasfutebol/dto/PartidaDto.java
@@ -1,4 +1,23 @@
 package com.meli.hseixas.listaspartidasfutebol.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class PartidaDto {
+
+    private Long id;
+
+    private String clubeMandante;
+
+    private String clubeVisitante;
+
+    private int placarMandante;
+
+    private int placarVisitante;
+
+    private LocalDateTime horario;
+
+    private String nomeEstadio;
 }
